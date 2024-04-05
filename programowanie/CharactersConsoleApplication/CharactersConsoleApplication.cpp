492e1e3136c9ee2998bb7cc4f8b1f900a22b72e1
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-05 14:24:26 +0200

    Zadania - length, char

--- programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
+++ programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -67,12 +67,69 @@ void task4()
 //Napisz program, który wczyta ³añcuch znaków i policzy ile jest ma³ych liter 'a'.
 void task5()
 {
+    string text;
+    cout << "Napisz cos: \n";
+    cin >> text;
 
+    int length = text.length();
+
+    //cout << "Ostatni znak w tekscie: " << text[length - 1] << endl;
+
+    int counter = 0;
+
+    for (int i = 0; i < length; i++) {
+        if (text[i] == 'a')
+            counter++;
+    }
+    cout << "Ilosc liczb a: " << counter;
+}
+// Napisz program ktory bedzie prosil o haslo i nie przepusci dopoki nie bedzie ono prawidlowe
+void task6()
+{
+    string password = "abc123";
+    string userGuess;
+
+    do {
+        cout << "Podaj haslo: ";
+        cin >> userGuess;
+    } while (userGuess != password);
+    cout << "Zgadles haslo!";
+}
+// Napisz program ktory pobiera od uzytkownika ciag znakow i wyswietla liczbe samoglosek i spolglosek
+void task7()
+{
+    string userInput;
+    cout << "Napisz cos: " << endl;
+    cin >> userInput;
+
+    int counter = 0;
+    int counter2 = 0;
+
+    int length = userInput.length();
+
+    for (int letter = 0; letter <= length; letter++) {
+        if (userInput[letter] == 'a' ||
+            userInput[letter] == 'e' ||
+            userInput[letter] == 'i' ||
+            userInput[letter] == 'o' ||
+            userInput[letter] == 'u' ||
+            userInput[letter] == 'y') {
+            counter++;
+        }
+        else {
+            counter2++;
+        }
+    }
+    cout << "Liczba samoglosek: " << counter;
+    cout << "Liczba spolglosek: " << counter2;
 }
 int main()
 {
    /* task1();*/
    /* task2();*/
    /* task3();*/
-    task4();
+   /* task4();*/
+   /* task5();*/
+   /* task6();*/
+    task7();
 }

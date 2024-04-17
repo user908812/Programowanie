75d2e198f5ddcb3bd8b280cbb0e654c3248c8ad3
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-17 10:42:32 +0200

    Tablice - zadania

--- programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+// Napisz program, ktory wczyta np. 5 liczb
+// a nastepnie wyswietli je w odwrotnej kolejnosci.
 void task1()
 {
     /*
@@ -29,7 +31,34 @@ void task1()
     }
     cout << endl;
 }
+// Napisz program obliczajacy srednia arytmetyczna elementow w tablicy liczb calkowitych.
+void task2()
+{
+    const unsigned int ARRAY_SIZE = 5;
+    int numbers[ARRAY_SIZE];
+
+    srand(time(0));
+    cout << "Liczby w kolekcji: " << endl;
+
+    for (int n = 0; n < ARRAY_SIZE; n++) 
+    {
+        numbers[n] = rand() % 11; // <-- liczby od 0 do 100
+        cout << numbers[n] << ", ";
+    }
+    cout << "\n\n";
+
+    int sum = numbers[0];
+    for (int n = 0; n < ARRAY_SIZE; n++)
+    {
+        sum = sum + numbers[n];
+    }
+    double average = sum * 1.0 / ARRAY_SIZE;
+
+    cout << "Suma wynosi " << sum;
+    cout << "Srednia wynosi " << average;
+}
 int main()
 {
-    task1();
+ // task1();
+  task2();
 }

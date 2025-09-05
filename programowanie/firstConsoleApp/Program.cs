ce7410805a9761f563af27b1b1924a0f6dff6331
Sala213 <Sala213@ezse.pl>
2025-09-05 10:39:42 +0200

    Poczatek csharp

---
+++ programowanie/firstConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//float number = 9999999999;
+
+//for (float i = 0; i <= number; i++)
+//{
+//    Console.WriteLine("Witam");
+//}
+
+Console.Write("Hello world! \n");
+Console.WriteLine("Hello world!");
+
+const string name = "Jan";
+const string surname = "Kowalski";
+
+Console.WriteLine("Witaj " + name + " " + surname + "!");

cd9adc0681d3fb8cda100d18addd772f6b412bb2
Sala213 <Sala213@ezse.pl>
2025-09-09 14:55:24 +0200

    Male zmiany w csharp

--- programowanie/firstConsoleApp/Program.cs
+++ programowanie/firstConsoleApp/Program.cs
@@ -11,5 +11,28 @@ Console.WriteLine("Hello world!");
 
 const string name = "Jan";
 const string surname = "Kowalski";
+const double age = 25.5;
+int speed = 80;
 
-Console.WriteLine("Witaj " + name + " " + surname + "!");
+Console.WriteLine("Witaj {1} {0}! Masz {2} lat.", name, surname, age); // Witaj Kowalski Jan! Masz 25,5 lat.
+Console.WriteLine($"Predkosc: {speed} km\\h");
+
+int firstNumber = 5;
+int secondNumber = firstNumber;
+
+secondNumber = 6;
+
+Console.WriteLine($"firstNumber: {firstNumber}");
+Console.WriteLine($"secondNumber: {secondNumber}");
+
+void ParameterTest_v1(int p)
+{
+    Console.WriteLine($"Przed zmianą ParameterTest_v1: {p}");
+    p++;
+    Console.WriteLine($"Po zmianie ParameterTest_v1: {p}");
+}
+
+firstNumber = 99;
+Console.WriteLine($"Przed ParameterTest_v1: {firstNumber}");
+ParameterTest_v1(firstNumber);
+Console.WriteLine($"Po ParameterTest_v1: {firstNumber}");

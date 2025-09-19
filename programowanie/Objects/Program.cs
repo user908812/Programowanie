d5e244b9ba3f2d2644be38ecc43cee6a939a5c43
Sala213 <Sala213@ezse.pl>
2025-09-19 10:43:16 +0200

    Nowe funkcje parameterTest

--- programowanie/Objects/Program.cs
+++ programowanie/Objects/Program.cs
@@ -32,7 +32,7 @@ Console.WriteLine($"Punkt pierwszy po ParameterTestStruct_v1 ma: ({firstPoint.x}
 ParameterTestStruct_v2(ref firstPoint);
 Console.WriteLine($"Punkt pierwszy po ParameterTestStruct_v2 ma: ({firstPoint.x}, {firstPoint.y})");
 
-//---------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 PointClass firstPointClass = new PointClass();
 firstPointClass.x = 5;
@@ -45,3 +45,33 @@ secondPointClass.x = 20;
 
 Console.WriteLine($"firstPointClass ma: ({firstPointClass.x}, {firstPointClass.y})");
 Console.WriteLine($"secondPointClass ma: ({secondPointClass.x}, {secondPointClass.y})");
+
+void ParameterTestClass_v1(PointClass pc)
+{
+    Console.WriteLine($"W ParameterTestClass_v1 punkt ma: ({pc.x}, {pc.y})");
+    pc.x = 99;
+    Console.WriteLine($"W ParameterTestClass_v1 punkt ma: ({pc.x}, {pc.y})");
+}
+void ParameterTestClass_v2(PointClass pc)
+{
+    Console.WriteLine($"W ParameterTestClass_v2 punkt ma: ({pc.x}, {pc.y})");
+    pc = new PointClass();
+    pc.x = 150;
+    Console.WriteLine($"W ParameterTestClass_v2 punkt ma: ({pc.x}, {pc.y})");
+}
+void ParameterTestClass_v3(ref PointClass pc)
+{
+    Console.WriteLine($"W ParameterTestClass_v3 punkt ma: ({pc.x}, {pc.y})");
+    pc = new PointClass();
+    pc.x = 42;
+    Console.WriteLine($"W ParameterTestClass_v3 punkt ma: ({pc.x}, {pc.y})");
+}
+
+ParameterTestClass_v1(firstPointClass);
+Console.WriteLine($"firstPointClass: ({firstPointClass.x}, {firstPointClass.y})");
+
+ParameterTestClass_v2(firstPointClass);
+Console.WriteLine($"firstPointClass: ({firstPointClass.x}, {firstPointClass.y})");
+
+ParameterTestClass_v3(ref firstPointClass);
+Console.WriteLine($"firstPointClass: ({firstPointClass.x}, {firstPointClass.y})");

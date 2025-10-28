27a907710073b2b72871b2640d75c557c4a65845
Sala213 <Sala213@ezse.pl>
2025-10-28 15:06:30 +0100

    Zadanie 1, zrob zadanie 2 z egzaminu

--- programowanie/egzamin/Program.cs
+++ programowanie/egzamin/Program.cs
@@ -1,4 +1,7 @@
 ﻿using egzamin;
 
-Task1 task1 = new Task1();
-task1.Task();
+//Task1 task1 = new Task1();
+//task1.Task();
+
+Task1.NWD testNWD = new Task1.NWD();
+testNWD.Oblicz(12, 36);
--- programowanie/egzamin/Task1.cs
+++ programowanie/egzamin/Task1.cs
@@ -6,39 +6,63 @@ using System.Threading.Tasks;
 
 namespace egzamin
 {
+
     internal class Task1
     {
-        public void Task()
+
+        public class NWD
         {
-
-            Console.WriteLine("Wpisz a:");
-            int a = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Wpisz b:");
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            int result;
-
-            if (!(a < 0) && !(b < 0))
+            public void Oblicz(int a, int b)
             {
-                if (a != b)
+                if (a < 0 || b < 0) Console.WriteLine("Liczby maja byc dodatnie");
+
+                Console.WriteLine($"NWD Liczb {a} i {b} to:");
+
+                while (b != 0)
                 {
-                    if (a > b)
-                    {
-                        result = a /= a - b;
-                        Console.WriteLine("Wynik: " + result);
-                    }
-                    else
-                    {
-                        result = b /= b - a;
-                        Console.WriteLine("Wynik: " + result);
-                    }
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
                 }
-                else
-                {
-                    Console.WriteLine("Liczba a: " + a);
-                }
+
+                Console.WriteLine(a);
             }
-        } 
+        }
+
+        
     }
+
+    //public void Task()
+    //{
+
+    //    Console.WriteLine("Wpisz a:");
+    //    int a = Convert.ToInt32(Console.ReadLine());
+
+    //    Console.WriteLine("Wpisz b:");
+    //    int b = Convert.ToInt32(Console.ReadLine());
+
+    //    int result;
+
+    //    if (!(a < 0) && !(b < 0))
+    //    {
+    //        if (a != b)
+    //        {
+    //            if (a > b)
+    //            {
+    //                result = a /= a - b;
+    //                Console.WriteLine("Wynik: " + result);
+    //            }
+    //            else
+    //            {
+    //                result = b /= b - a;
+    //                Console.WriteLine("Wynik: " + result);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Liczba a: " + a);
+    //        }
+    //    }
+    //} 
+
 }
